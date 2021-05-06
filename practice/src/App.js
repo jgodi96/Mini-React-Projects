b2da1a6574ffd19782fbb78e9a697d885cdc50f8
Joshua Godinez <jgodi96@gmail.com>
2021-05-06 13:05:23 -0700

    Finished Practice

---
+++ practice/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [usersList, setUserList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevUsersList) => {
+      return [...prevUsersList, { id:Math.random().toString(), name: uName, age: uAge }];
+    });
+  };
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
