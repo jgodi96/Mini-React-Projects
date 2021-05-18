8fc54462b45a008f770d634b85ea081901d9f193
Joshua Godinez <jgodi96@gmail.com>
2021-05-18 11:51:53 -0700

    Meal display

---
+++ FoodApp/src/components/Meals/MealsSummary.js
@@ -0,0 +1,9 @@
+import classes from './MealsSummary.module.css'
+const MealsSummary = () => {
+    return <section className={classes.summary}>
+        <h2>Delicious Food Delivered to you</h2>
+        <p>Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.</p>
+        <p>All of our meals are cooked with hight-quality ingredients.</p>
+    </section>
+}
+export default MealsSummary;
