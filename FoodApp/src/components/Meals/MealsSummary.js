9c1e5370a40115fefb253b3bfff3081ff40c0d5f
jgodi96@gmail.com <jgodi96@gmail.com>
2021-12-16 13:32:15 -0800

    react bas

--- FoodApp/src/components/Meals/MealsSummary.js
+++
@@ -1,9 +0,0 @@
-import classes from './MealsSummary.module.css'
-const MealsSummary = () => {
-    return <section className={classes.summary}>
-        <h2>Delicious Food Delivered to you</h2>
-        <p>Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.</p>
-        <p>All of our meals are cooked with hight-quality ingredients.</p>
-    </section>
-}
-export default MealsSummary;
